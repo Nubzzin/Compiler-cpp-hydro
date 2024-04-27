3d0e7cf09a3a2289039fba35dafc0a0c5c57e722
nubsdre <matheus.thelas@hotmail.com>
2024-04-26 21:29:41 -0300

    Funcionou porra, tokens
    d
    a

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "tokenizer.h"
+
+int main(int argc, char *argv[]) {
+  std::fstream file("test.hy");
+  std::stringstream buf;
+  {
+    if (!file.is_open()) {
+      std::cerr << "Falha ao encontrar arquivo!" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    std::string str_file;
+    std::getline(file, str_file);
+    buf << str_file;
+  }
+
+  Tokenizer tokenizer(buf.str());
+  std::vector<Token> tokens = tokenizer.tokenize();
+
+  std::cout << tokens.size() << std::endl;
+  return 0;
+}
---
+++ src/tokenizer.h
@@ -0,0 +1,90 @@
+#ifndef _TOKENIZER_H_
+#define _TOKENIZER_H_
+
+#include <cctype>
+#include <cstdlib>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+
+enum class TokenType {
+  Exit,
+  IntLit,
+  SemiCo,
+};
+
+struct Token {
+  TokenType type;
+  std::optional<std::string> value;
+};
+
+class Tokenizer {
+private:
+  const std::string src;
+  size_t index{};
+  std::string buf;
+
+  std::optional<char> peek(size_t offset = 0) {
+    if (src[index + offset]) {
+      return src[index + offset];
+    } else {
+      return {};
+    }
+  }
+
+  std::optional<char> consume() {
+    if (!src[index++]) {
+      return src[index++];
+    } else {
+      return {};
+    }
+  }
+
+public:
+  Tokenizer(std::string src) : src{src} {}
+  ~Tokenizer() = default;
+
+  std::vector<Token> tokenize() {
+    std::vector<Token> tokens;
+    std::string buf;
+
+    while (peek().has_value()) {
+      if (std::isalpha(peek().value())) {
+        buf.push_back(peek().value());
+        consume();
+        while (std::isalnum(peek().value())) {
+          buf.push_back(peek().value());
+          consume();
+        }
+
+        if (buf == "exit") {
+          tokens.push_back({.type = TokenType::Exit});
+          buf.clear();
+        }
+      } else if (std::isdigit(peek().value())) {
+        buf.push_back(peek().value());
+        consume();
+        while (std::isdigit(peek().value())) {
+          buf.push_back(peek().value());
+          consume();
+        }
+        tokens.push_back({.type = TokenType::IntLit, .value = buf});
+        buf.clear();
+      } else if (peek().value() == ';') {
+        tokens.push_back({.type = TokenType::SemiCo});
+        consume();
+        buf.clear();
+      } else if (std::isspace(peek().value())) {
+        consume();
+        continue;
+      } else {
+        std::cerr << "Syntax error!" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+    }
+    return tokens;
+  }
+};
+
+#endif
